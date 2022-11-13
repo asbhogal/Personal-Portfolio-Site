7eb756c69f43ea426c8c3b0665f4379c56b13d53
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-11-13 21:12:12 +0000

    omitted obsolete code and files

--- src/js/nav-menu.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('#nav-bar-btn').onclick = function (e) {
-    const menu = document.querySelector('#nav-menu');
-    const btn = document.querySelector('#nav-bar-btn');
-
-    menu.classList.toggle('is-active');
-    btn.classList.toggle('is-active');
-
-    e.preventDefault();
-}
