eed7e4031c0e2d7597a647cf3d03c0083e913d03
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-01-26 11:09:13 +0000

    uninstalled parcel, installed webpack, react and dev dep.

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const   path = require("path"),
+        MiniCSSExtractPlugin = require("mini-css-extract-plugin"),
+        HTMLWebpackPlugin = require("html-webpack-plugin"),
+        { CleanWebpackPlugin } = require("clean-webpack-plugin"),
+        ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin"),
+        plugins = [
+            new CleanWebpackPlugin(),
+            new MiniCSSExtractPlugin(),
+            new HTMLWebpackPlugin({
+                title: "Personal Portfolio Site",
+                author: "Aman Singh Bhogal",
+                template: "./src/index.html"
+            }),
+        ];
+
+let     mode = "development";
+
+process.env.NODE_ENV === "production" ? mode = "production" : plugins.push(new ReactRefreshWebpackPlugin());
+
+module.exports = {
+    mode: mode,
+
+    entry: {
+        main: './src/js/index.js',
+    },
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        assetModuleFilename: "images/[hash][ext][query]"
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset"
+            },
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [
+                    MiniCSSExtractPlugin.loader, 
+                    "css-loader", 
+                    "postcss-loader",
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                type: "asset/resource",
+            },
+        ],
+    },
+
+    plugins: plugins,
+
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+
+    devtool: "source-map",
+    devServer: {
+        static: "./dist",
+        hot: true
+    },
+};
