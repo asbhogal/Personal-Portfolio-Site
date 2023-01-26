c2c9b6805d993bb692e6a1842a9e89db628b798b
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-01-26 20:34:15 +0000

    added font stylesheets and started typography styling

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,16 @@
+import chevronRight from "../assets/images/icons/ChevronRight.svg"
+
 const Container = () => {
     return (
-        <div className="MainContainer"></div>
+        <div className="MainContainer">
+            <section className="MainHeadline">
+                <h1>Creating bold, elegant and humanistic interfaces &#124; est. 2020</h1>
+            </section>
+            <section className="CTAButtons">
+                <button className="ViewProjects">projects<img src={ chevronRight }></img></button>
+                <button className="Contact">contact<img src={ chevronRight } ></img></button>
+            </section>
+        </div>
     )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,6 +7,7 @@ const Home = () => {
     return (
         <>
         <Navbar />
+        <Container />
         <Footer />
         </>
     )
