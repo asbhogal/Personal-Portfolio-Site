4dc961903974012c2589cb7bee041dd3cbc84f72
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-01-26 14:28:25 +0000

    added responsive parameters - media queries

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 const Navbar = () => {
     return (
         <div className="Navbar">
-            <div className="HamburgerMenu"></div>
+            <div className="HamburgerMenu">
+                <span className="NavbarLine"></span>
+                <span className="NavbarLine"></span>
+                <span className="NavbarLine"></span>
+            </div>
             <div className="MainLogo"></div>
             <div className="HireCTAButton"></div>
         </div>
