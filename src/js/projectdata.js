b9791c8b97dde10ee7e65cd31bb978877b59bb08
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-01-27 14:41:23 +0000

    added projectdata file

---
+++ src/js/projectdata.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        id: 1,
+        title: "Kadampa Meditation Centre Birmingham",
+        description: "",
+    },
+    {
+        id: 2,
+        title: "Glenalnarch Distillery",
+        description: "",
+    }  
+];
