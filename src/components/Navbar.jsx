4a8964bd752d76523e0193e64427513b8dfd2db9
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-01-28 17:33:07 +0000

    started nav bar menu toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,31 @@
 import mainLogo from "../assets/images/logos/Personal Brand Logo (Latest)-Transparent-Bg.svg"
+import { useState } from "react";
 
 const Navbar = () => {
+
+    const   [HamburgerClass, setHamburgerClass] = useState("HamburgerBar Unclicked"),
+            [MenuClass, setMenuClass] = useState("FullScreenNavMenuContainer Hidden"),
+            [isMenuClicked, setIsMenuClicked] = useState(false);
+
+    const   updateMenu = () => {
+        if(!isMenuClicked) {
+            setHamburgerClass("HamburgerBar Clicked")
+            setMenuClass("FullScreenNavMenuContainer Visible")
+        }
+        else {
+            setHamburgerClass("HamburgerBar Unclicked")
+            setMenuClass("FullScreenNavMenuContainer Hidden")
+        }
+    }
+
     return (
         <nav className="Navbar">
             <div className="HamburgerMenu">
-                <span className="NavbarLine"></span>
-                <span className="NavbarLine"></span>
-                <span className="NavbarLine"></span>
+                <span className={ HamburgerClass } onClick={ updateMenu }></span>
+                <span className={ HamburgerClass } onClick={ updateMenu }></span>
+                <span className={ HamburgerClass } onClick={ updateMenu }></span>
             </div>
+            <div className={ MenuClass }></div>
             <div className="MainLogo">
                 <img src={ mainLogo }></img>
             </div>
