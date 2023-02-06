00eba5ad0f9714042e28df607b43cd045f0a8f16
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-02-06 21:23:18 +0000

    further changes to style and positioning

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,10 +12,12 @@ const Projects = () => {
                 <div className="ProjectSection">
                     <div className="ProjectInfo">
                         <h2>Kadampa <span className="FirstLetterAccent">M</span>editation <span className="FirstLetterAccent">C</span>entre <span className="FirstLetterAccent">B</span>irmingham</h2>
-                        <span>May 2020 - Feb 2021</span>
-                        <p>Kadampa Meditation Centre is part of a nonprofit organisation which delivers a modern form of Buddhism to their local community. When the pandemic started, their centre closed and they required a revamp of their current site to allow class and course bookings and membership subscriptions. The site was designed and developed using Astra Pro and Elementor Pro with WooCommerce and Stripe. The site was successfully launched on 19th December 2020."</p>
-                        <h3>Design Brief</h3>
-                        <p>"The Resident Teacher required a site which provided a ‘window’ to the center for people; making class and course sessions and doctrine accessible for everyone. Information provided by them were used to populate the pages. An alternative version of the logo was located within the archives and chosen for it’s rounded vectors and soothing appearance. Accents derived from this dictated the color scheme.</p>
+                        <span className="ProjectTimeDuration">May 2020 - Feb 2021</span>
+                        <p className="ProjectSummary">Kadampa Meditation Centre is part of a nonprofit organisation which delivers a modern form of Buddhism to their local community. When the pandemic started, their centre closed and they required a revamp of their current site to allow class and course bookings and membership subscriptions. The site was designed and developed using Astra Pro and Elementor Pro with WooCommerce and Stripe. The site was successfully launched on 19th December 2020."</p>
+                        <div className="DesignBrief">
+                            <h3>Design Brief</h3>
+                            <p className="DesignBriefSummary">"The Resident Teacher required a site which provided a ‘window’ to the center for people; making class and course sessions and doctrine accessible for everyone. Information provided by them were used to populate the pages. An alternative version of the logo was located within the archives and chosen for it’s rounded vectors and soothing appearance. Accents derived from this dictated the color scheme.</p>
+                        </div>
                         <div className="Guides">
                             <div className="BrandGuideSection">
                                 <h3>Brand Guide</h3>
@@ -35,32 +37,32 @@ const Projects = () => {
                                     </div>
                                     <div className="Color-2">
                                     <div className="ColorTitle">
-                                            <p>01</p>
-                                            <p>chinese violet</p>
+                                            <p>02</p>
+                                            <p>mountbatten pink</p>
                                         </div>
                                         <div className="ColorInfo">
-                                            <p><span>hex</span>&#58; 856187</p>
-                                            <p><span>rgb</span>&#58; 133, 97, 135</p>
+                                            <p><span>hex</span>&#58; 98719D</p>
+                                            <p><span>rgb</span>&#58; 152, 113, 157</p>
                                         </div>
                                     </div>
                                     <div className="Color-3">
                                     <div className="ColorTitle">
-                                            <p>01</p>
-                                            <p>chinese violet</p>
+                                            <p>03</p>
+                                            <p>african violet</p>
                                         </div>
                                         <div className="ColorInfo">
-                                            <p><span>hex</span>&#58; 856187</p>
-                                            <p><span>rgb</span>&#58; 133, 97, 135</p>
+                                            <p><span>hex</span>&#58; B594C3</p>
+                                            <p><span>rgb</span>&#58; 181, 148, 195</p>
                                         </div>
                                     </div>
                                     <div className="Color-4">
                                     <div className="ColorTitle">
-                                            <p>01</p>
-                                            <p>chinese violet</p>
+                                            <p>04</p>
+                                            <p>thistle</p>
                                         </div>
                                         <div className="ColorInfo">
-                                            <p><span>hex</span>&#58; 856187</p>
-                                            <p><span>rgb</span>&#58; 133, 97, 135</p>
+                                            <p><span>hex</span>&#58; D0BDDB</p>
+                                            <p><span>rgb</span>&#58; 208, 189, 219</p>
                                         </div>
                                     </div>
                                 </div>
