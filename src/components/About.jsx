7f8b2680639a4160fe80c34a1c697fa2fb0c94dd
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-02-06 13:12:35 +0000

    further changes and amendments

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,15 +8,18 @@ const About = () => {
         <>
             <Navbar />
             <h1 className="PageHeaderTitle">&#38;&#46;about</h1>
-            <h2 id="AboutPageTitle1" className="PageSubTitleText">  figma &#47;
-                                                                    adobe ai &#47;
-                                                                    adobe ps &#47;
-                                                                    adobe xd &#47; </h2>
+            <h2 id="AboutPageTitle1" className="PageSubTitleText">  
+                figma &#47;
+                adobe ai &#47;
+                adobe ps &#47;
+                adobe xd &#47; 
+            </h2>
             <h2 id="AboutPageTitle2" className="PageSubTitleText">
-                                                                    react &#47;
-                                                                    sass &#47;
-                                                                    git &#47;
-                                                                    webpack</h2>
+                react &#47;
+                sass &#47;
+                git &#47;
+                webpack
+            </h2>
             <p className="AboutText">AMAN SINGH BHOGAL is a UI Engineer with 2 1/2 years of client side experience in Front End Development. His work covers building CMS-based websites (WordPress and Hubspot) and static applications using vanilla HTML5, CSS3, SASS and JavaScript. He now works with international clients to design and develop interfaces using Adobe CC, Figma and React from existing and new design briefs, as well as offering consultancies.</p>
             <CTAButtons />
             <Footer />
--- src/js/projectdata.js
+++ src/js/projectdata.js
@@ -5,7 +5,10 @@ export default [
         description: "Kadampa Meditation Centre is part of a nonprofit organisation which delivers a modern form of Buddhism to their local community. When the pandemic started, their centre closed and they required a revamp of their current site to allow class and course bookings and membership subscriptions. The site was designed and developed using Astra Pro and Elementor Pro with WooCommerce and Stripe. The site was successfully launched on 19th December 2020.",
         brief: "The Resident Teacher required a site which provided a ‘window’ to the center for people; making class and course sessions and doctrine accessible for everyone. Information provided by them were used to populate the pages. An alternative version of the logo was located within the archives and chosen for it’s rounded vectors and soothing appearance. Accents derived from this dictated the color scheme.",
         brandGuide: "PATH TO IMG FILE",
-        
+        swatches: {
+            1: '#856187',
+            2: '#98719D'
+        }
     },
     {
         id: 2,
