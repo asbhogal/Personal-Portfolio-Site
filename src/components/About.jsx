09ae282a857981ee66db257cd1f61861a2724abc
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-02-08 14:56:53 +0000

    changes to content, styling and responsive of about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,8 @@ const About = () => {
             <Header />
                 <section className="AboutSection">
                     <h1 className="PageHeaderTitle">&#38;&#46;about</h1>
-                    <div className="AboutSection">
+                    <div className="HorizontalDivider"></div>
+                    <div className="AboutContent">
                         <div id="TechSkills" className="ExperienceList">
                             <p id="AboutPageTitle1" className="PageSubTitleText">  
                                 Figma &#124; Adobe Ai &#124; Adobe PS &#124; Adobe xD
@@ -22,7 +23,7 @@ const About = () => {
                                 Responsive Web Design &#124; Low & High-Fidelity Wireframing &#124; Components &#124; UI Prototyping
                             </p>
                         </div>
-                    <p className="AboutText"><span class="FirstLetterAccent">A</span>MAN SINGH BHOGAL is a UI Engineer with 2 1/2 years of client side experience in Front End Development. His work covers building CMS-based websites (WordPress and Hubspot) and static applications using vanilla HTML5, CSS3, SASS and JavaScript. He now works with international clients to design and develop interfaces using Adobe CC, Figma and React from existing and new design briefs, as well as offering consultancies.</p>
+                    <p className="AboutText">A MAN SINGH BHOGAL is a UI Engineer with 2 1/2 years of client side experience in Front End Development. His work covers building CMS-based websites (WordPress and Hubspot) and static applications using vanilla HTML5, CSS3, SASS and JavaScript. He now works with international clients to design and develop interfaces using Adobe CC, Figma and React from existing and new design briefs, as well as offering consultancies.</p>
                 </div>
                 </section>
             <Footer />
