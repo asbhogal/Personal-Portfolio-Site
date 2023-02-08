56303a1a00985f8a5966c0b8919cf6f6699ff2bf
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-02-08 14:03:49 +0000

    further changes to styles and content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,32 +1,30 @@
 import "../scss/index.scss";
 import Header from "./Header";
 import Footer from "./Footer";
-import CTAButtons from "./Buttons";
 
 const About = () => {
     return (
         <>
             <Header />
-            <main className="MainContainer">
-                <div className="AboutCover">
+                <section className="AboutSection">
                     <h1 className="PageHeaderTitle">&#38;&#46;about</h1>
+                    <div className="AboutSection">
+                        <div id="TechSkills" className="ExperienceList">
+                            <p id="AboutPageTitle1" className="PageSubTitleText">  
+                                Figma &#124; Adobe Ai &#124; Adobe PS &#124; Adobe xD
+                            </p>
+                            <p id="AboutPageTitle2" className="PageSubTitleText">
+                                React &#124; SASS &#124; Git &#124; Webpack
+                            </p>
+                        </div>
+                        <div id="DesignSkills" className="ExperienceList">
+                            <p id="AboutPageTitle1" className="PageSubTitleText">  
+                                Responsive Web Design &#124; Low & High-Fidelity Wireframing &#124; Components &#124; UI Prototyping
+                            </p>
+                        </div>
+                    <p className="AboutText"><span class="FirstLetterAccent">A</span>MAN SINGH BHOGAL is a UI Engineer with 2 1/2 years of client side experience in Front End Development. His work covers building CMS-based websites (WordPress and Hubspot) and static applications using vanilla HTML5, CSS3, SASS and JavaScript. He now works with international clients to design and develop interfaces using Adobe CC, Figma and React from existing and new design briefs, as well as offering consultancies.</p>
                 </div>
-                <div className="AboutSection">
-                    <p id="AboutPageTitle1" className="PageSubTitleText">  
-                        figma &#47;
-                        adobe ai &#47;
-                        adobe ps &#47;
-                        adobe xd &#47; 
-                    </p>
-                    <p id="AboutPageTitle2" className="PageSubTitleText">
-                        react &#47;
-                        sass &#47;
-                        git &#47;
-                        webpack
-                    </p>
-                    <p className="AboutText">AMAN SINGH BHOGAL is a UI Engineer with 2 1/2 years of client side experience in Front End Development. His work covers building CMS-based websites (WordPress and Hubspot) and static applications using vanilla HTML5, CSS3, SASS and JavaScript. He now works with international clients to design and develop interfaces using Adobe CC, Figma and React from existing and new design briefs, as well as offering consultancies.</p>
-                </div>
-            </main>
+                </section>
             <Footer />
         </>
     )
