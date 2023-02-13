04e8d252b782cae9695cf5e1503b094544585d5e
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-02-13 14:11:11 +0000

    added security header to vercel.json config

---
+++ src/components/Container.jsx
@@ -0,0 +1,12 @@
+const Container = () => {
+    return (
+        <div className="MainContainer">
+            <section className="MainHeadline">
+                <h1 className="MainHomeTitle">Creating bold, elegant &#38;
+ humanistic interfaces &#124; est. 2020</h1>
+            </section>
+        </div>
+    )
+}
+
+export default Container;
