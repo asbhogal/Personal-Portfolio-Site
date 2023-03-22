aad46f9faf35d1fd34ad615be846f1e55f1e7e53
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-03-22 13:27:17 +0000

    further changes to content and styling

--- src/components/BrandGuide.jsx
+++ src/components/BrandGuide.jsx
@@ -23,23 +23,25 @@ const BrandGuide = () => {
                 <p>Case</p>
                 <p>Sentence</p>
             </div>
-            <p>Weights</p>
-            <ul className="BrandProjectTypography TypographyList">
-                <div className="TypographyWeightSet">
-                    <p id="BrandTypography-400" className="BrandProjectTypography TypographyListItem-400">Regular</p>
-                    <p id="BrandTypography-500" className="BrandProjectTypography TypographyListItem-500">Medium</p>
-                    <p id="BrandTypography-600" className="BrandProjectTypography TypographyListItem-600">Semi Bold</p>
-                    <p id="BrandTypography-700" className="BrandProjectTypography TypographyListItem-700">Bold</p>
-                    <p id="BrandTypography-900" className="BrandProjectTypography TypographyListItem-900">Black</p>
-                </div>
-                <div className="TypographyFontSet">
-                    <li id="BrandTypography-400" className="BrandProjectTypography TypographyListItem-400">ABC abc 123</li>
-                    <li id="BrandTypography-500" className="BrandProjectTypography TypographyListItem-500">ABC abc 123</li>
-                    <li id="BrandTypography-600" className="BrandProjectTypography TypographyListItem-600">ABC abc 123</li>
-                    <li id="BrandTypography-700" className="BrandProjectTypography TypographyListItem-700">ABC abc 123</li>
-                    <li id="BrandTypography-900" className="BrandProjectTypography TypographyListItem-900">ABC abc 123</li>
-                </div>
-            </ul>
+            <div className="TypographyStyles">
+                <p className="WeightsTitle">Weights</p>
+                <ul className="BrandProjectTypography TypographyList">
+                    <div className="TypographyWeightSet">
+                        <p id="BrandTypography-400" className="BrandProjectTypography TypographyListItem-400">Regular</p>
+                        <p id="BrandTypography-500" className="BrandProjectTypography TypographyListItem-500">Medium</p>
+                        <p id="BrandTypography-600" className="BrandProjectTypography TypographyListItem-600">Semi Bold</p>
+                        <p id="BrandTypography-700" className="BrandProjectTypography TypographyListItem-700">Bold</p>
+                        <p id="BrandTypography-900" className="BrandProjectTypography TypographyListItem-900">Black</p>
+                    </div>
+                    <div className="TypographyFontSet">
+                        <li id="BrandTypography-400" className="BrandProjectTypography TypographyListItem-400">ABC abc 123</li>
+                        <li id="BrandTypography-500" className="BrandProjectTypography TypographyListItem-500">ABC abc 123</li>
+                        <li id="BrandTypography-600" className="BrandProjectTypography TypographyListItem-600">ABC abc 123</li>
+                        <li id="BrandTypography-700" className="BrandProjectTypography TypographyListItem-700">ABC abc 123</li>
+                        <li id="BrandTypography-900" className="BrandProjectTypography TypographyListItem-900">ABC abc 123</li>
+                    </div>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
