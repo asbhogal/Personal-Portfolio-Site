c003f8524f7121b208ebdb9289c8e57772e75290
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-04-10 15:09:50 +0100

    integrated carbon badge

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,9 @@
-import { MdAlternateEmail } from "react-icons/md";
 import { FaLinkedinIn } from "react-icons/fa";
 import { BsGithub } from "react-icons/bs";
-import { BsInstagram } from "react-icons/bs";
 import { FiFigma } from "react-icons/fi";
 
 import { motion as m } from "framer-motion"
+import { WebsiteCarbonBadge } from "react-websitecarbon-badge";
 
 const Footer = () => {
     return (
@@ -12,18 +11,13 @@ const Footer = () => {
             <div className="Copyright">
                 <p>©Aman Singh Bhogal - 2023</p>
             </div>
+            <WebsiteCarbonBadge />
             <div className="SocialIcons">
-                <a href="mailto:business@amansinghbhogal.com" aria-label="Contact Aman by email">
-                    <MdAlternateEmail />
-                </a>
                 <a target="_blank" href="https://www.linkedin.com/in/amansinghbhogal" aria-label="Visit Aman's LinkedIn page">
                     <FaLinkedinIn />
                 </a>
                 <a target="_blank" href="https://www.github.com/asbhogal" aria-label="Visit Aman's GitHub page">
                     <BsGithub />
-                </a>
-                <a target="_blank" href="https://www.instagram.com/amanuiux" aria-label="Visit Aman's Instagram page">
-                    <BsInstagram />
                 </a>
                 <a target="_blank" href="https://www.figma.com/@amansinghbhogal" aria-label="Visit Aman's Figma profile">
                     <FiFigma />
