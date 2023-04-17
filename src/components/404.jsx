add8b4eb7c3fa196bcf2c20c4bba530a2206db70
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-04-17 01:14:46 +0100

    improvements to 404 and additional services page styling and structure

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -1,6 +1,8 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { motion as m } from "framer-motion";
 
 const errorPage = () => {
 
@@ -11,12 +13,16 @@ const errorPage = () => {
     return (
         <>
             <Header />
-                <section className="ErrorPage">
-                    <h1>404</h1>
-                    <div className="HorizontalDivider"></div>
-                    <h2>The Page you're looking for does not exist</h2>
-                    <p>Click below to return home</p>
-                </section>
+                <m.section 
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }} className="ErrorPage">
+                    <h1 className="PageHeaderTitle">404</h1>
+                    <div className="ErrorPageContainer">
+                        <h2>The Page you're looking for does not exist</h2>
+                        <p>Click below to return home</p>
+                        <Link to='/' className="ReturnHomeButton">home</Link>
+                    </div>
+                </m.section>
             <Footer />
         </>
     )
