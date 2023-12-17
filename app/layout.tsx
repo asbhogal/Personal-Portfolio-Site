d3a4160e2ea3f87ff9e4d5d981fa6cd7b60ac9ee
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-12-17 20:24:01 +0000

    Adds global styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="root">
         <Header />
         <Cursor />
         <ScrollToTop />
