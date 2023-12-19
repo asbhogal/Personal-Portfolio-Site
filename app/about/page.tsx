ec1d6ffda7adaf997ea27a1b27e210dba6cf2b13
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-12-19 00:51:15 +0000

    Adds metadata to About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,10 @@
 import AboutSection from "@/components/AboutSection";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About | Aman Singh Bhogal",
+  description: "About Aman Singh Bhogal",
+};
 
 export default function Page() {
   return <AboutSection />;
