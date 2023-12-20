52bcb0860ed34c72d6c209921796e6e90b8402ae
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-12-20 00:55:08 +0000

    Adds metadata to not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from "next";
 import Error from "@/components/Error";
+
+export const metadata: Metadata = {
+  title: "Error 404 | Aman Singh Bhogal",
+  description: "Error 404. The page you're looking for does not exist.",
+};
 
 export default function NotFound() {
   return <Error />;
