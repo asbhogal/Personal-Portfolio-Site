33ae3ad56f0b281f9c09f9c71c2b01c60f13326f
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-12-20 11:03:18 +0000

    Updates metadata, adds portfolio to a11y testing - PASS

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,5 +1,11 @@
 import Portfolio from "@/components/Portfolio";
 import PortfolioTitle from "@/components/PortfolioTitle";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Portfolio | Aman Singh Bhogal",
+  description: "A list of portfolio projects created by Aman Singh Bhogal",
+};
 
 export default function Page() {
   return (
--- tests/a11y.spec.ts
+++ tests/a11y.spec.ts
@@ -1,15 +1,26 @@
 import { test, expect } from "@playwright/test";
-import AxeBuilder from "@axe-core/playwright"; // 1
+import AxeBuilder from "@axe-core/playwright";
 
 test.describe("homepage", () => {
-  // 2
   test("should not have any automatically detectable accessibility issues", async ({
     page,
   }) => {
-    await page.goto("/"); // 3
+    await page.goto("/");
 
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze(); // 4
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
-    expect(accessibilityScanResults.violations).toEqual([]); // 5
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
+
+test.describe("portfolio", () => {
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    await page.goto("/portfolio");
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
