29bc4bce22406b5fd18388a2e3ec3ea8dbb02ba7
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-12-21 21:25:45 +0000

    Abstracts away dynamic page data, switches to server-side data fetching, adds metadata

---
+++ utils/functions.ts
@@ -0,0 +1,20 @@
+export async function getProjectData() {
+  const res = await fetch(`${process.env.API_BASE_URL}/api/projects`);
+
+  if (!res.ok) {
+    throw new Error("Error fetching project data");
+  }
+
+  return res.json();
+}
+
+function titleCase(str: string) {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+export function generateProjectName(project: string) {
+  return `${titleCase(project)} | Aman Singh Bhogal`;
+}
