d0892df7e0a26159775271fe8f5d1a43e24b0427
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-12-22 17:57:38 +0000

    Adds SEO data and further a11y tests - all PASS

--- app/free-resources/page.tsx
+++ app/free-resources/page.tsx
@@ -1,6 +1,12 @@
 import { getResourcesData } from "@/utils/functions";
 import FreeResourcesSection from "./FreeResourcesSection";
 import FreeResourcesCover from "./FreeResourcesCover";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Free Resources | Aman Singh Bhogal",
+  description: "Free resources, including brand templates and wireframes",
+};
 
 export default async function Page() {
   const { resources: resourcesData } = await getResourcesData();
--- tests/a11y.spec.ts
+++ tests/a11y.spec.ts
@@ -36,3 +36,39 @@ test.describe("portfolio", () => {
     expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
+
+test.describe("additional services", () => {
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    await page.goto("/additional-services");
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
+
+test.describe("free resources", () => {
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    await page.goto("/free-resources");
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
+
+test.describe("testimonials", () => {
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    await page.goto("/testimonials");
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
