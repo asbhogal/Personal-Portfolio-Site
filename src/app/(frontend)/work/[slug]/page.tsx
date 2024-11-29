b3ab5d56cf50c300f6ef48a315ea4b1ffb1db8ba
Aman Bhogal <asbhogal1@live.co.uk>
2024-11-29 20:38:52 +0000

    feat: add dynamic work page

--- src/app/(frontend)/work/[slug]/page.tsx
+++ src/app/(frontend)/work/[slug]/page.tsx
@@ -1,7 +1,39 @@
-export default async function Page() {
+import { getPayload } from 'payload';
+import configPromise from '@payload-config';
+import { Metadata, ResolvingMetadata } from 'next';
+
+interface Props {
+  params: Promise<{ slug: string }>
+}
+
+export async function generateMetadata({ params }: Props, _: ResolvingMetadata): Promise<Metadata> {
+  const { slug } = await params;
+
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: 'projects',
+    depth: 2,
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  });
+
+  return {
+    description: data.docs.find((doc) => doc.slug === slug)?.description?.toString() || '',
+    title: `${data.docs.find((doc) => doc.slug === slug)?.title} | Aman Singh Bhogal`,
+  };
+}
+
+export default async function Page({ params }: Props) {
+  const { slug } = await params;
   return (
     <div>
-      <h1>Page</h1>
+      <h1>{slug}</h1>
     </div>
   );
 }
