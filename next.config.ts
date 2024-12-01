d9a7ad87ef09ce3ecb147639febef26461a943c0
Aman Bhogal <asbhogal1@live.co.uk>
2024-12-01 16:30:42 +0000

    fix: add rewrites for images in next config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        destination: 'https://www.amansinghbhogal.com/api/media/file/:path*',
+        source: '/api/media/file/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
