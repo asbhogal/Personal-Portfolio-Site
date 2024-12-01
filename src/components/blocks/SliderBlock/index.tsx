4e6cac95522928bad291b2e6c046818c7d828820
Aman Bhogal <asbhogal1@live.co.uk>
2024-12-01 18:27:30 +0000

    chore: update slider block ux

--- src/components/blocks/SliderBlock/index.tsx
+++ src/components/blocks/SliderBlock/index.tsx
@@ -4,6 +4,7 @@ import { Splide, SplideSlide } from '@splidejs/react-splide';
 import { Page } from '@/payload-types';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 import '@splidejs/react-splide/css';
 
@@ -13,40 +14,65 @@ interface Props {
   items: SliderBlock;
 }
 
-export const SliderBlock = ({ items }: Props) => (
-  <Splide
-    tag="div"
-    options={{
-      arrows: false,
-      autoplay: true,
-      direction: 'ttb',
-      height: '100dvh',
-      paginationDirection: 'ttb',
-      type: 'loop',
-      waitForTransition: true,
-      wheel: true,
-    }}
-  >
-    {items.cards?.map((item) => (
-      <SplideSlide
-        tag="div"
-        className={styles.splideSlide}
-        key={item.id}
-      >
-        <Link
-          title={item.title ?? ''}
-          href={`/work/${item.link}`}
+export const SliderBlock = ({ items }: Props) => {
+  const [direction, setDirection] = useState<'ltr' | 'rtl' | 'ttb' | 'btt'>('ttb');
+  const [wheel, setWheel] = useState<boolean | undefined>(undefined);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setDirection('ltr');
+        setWheel(false);
+      } else {
+        setDirection('ttb');
+        setWheel(true);
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <Splide
+      key={wheel}
+      tag="div"
+      options={{
+        arrows: false,
+        autoplay: true,
+        direction,
+        height: '100dvh',
+        paginationDirection: direction,
+        type: 'loop',
+        waitForTransition: true,
+        wheel,
+      }}
+    >
+      {items.cards?.map((item) => (
+        <SplideSlide
+          tag="div"
+          className={styles.splideSlide}
+          key={item.id}
         >
-          <Image
-            className={styles.image}
-            aria-label={item.title ?? ''}
-            src={typeof item.image === 'string' ? item.image : `/images/media/${item.image?.filename}`}
-            alt={typeof item.image === 'string' ? '' : item.image?.altText ?? ''}
-            width={676}
-            height={900}
-          />
-        </Link>
-      </SplideSlide>
-    ))}
-  </Splide>
-);
+          <Link
+            title={item.title ?? ''}
+            href={`/work/${item.link}`}
+          >
+            <Image
+              className={styles.image}
+              aria-label={item.title ?? ''}
+              src={typeof item.image === 'string' ? item.image : `/images/media/${item.image?.filename}`}
+              alt={typeof item.image === 'string' ? '' : item.image?.altText ?? ''}
+              width={676}
+              height={900}
+            />
+          </Link>
+        </SplideSlide>
+      ))}
+    </Splide>
+  );
+};
