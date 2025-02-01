32a14b188abc9ab778407b492524fa640b052d73
Aman Bhogal <asbhogal1@live.co.uk>
2025-02-01 17:23:24 +0000

    chore: update individual work page

--- src/app/(frontend)/work/[slug]/page.tsx
+++ src/app/(frontend)/work/[slug]/page.tsx
@@ -2,8 +2,8 @@ import { getPayload, PaginatedDocs } from 'payload';
 import configPromise from '@payload-config';
 import { Metadata, ResolvingMetadata } from 'next';
 import Image from 'next/image';
-import { ListBlock, ProjectShowcaseBlock, TypefaceBlock } from '@/src/components/blocks';
-import { RichText, VisuallyHidden } from '@/src/components/typography';
+import { ProjectShowcaseBlock, TypefaceBlock } from '@/src/components/blocks';
+import { RichText } from '@/src/components/typography';
 import { ArrowDown } from '@/src/components/graphics';
 import { Suspense } from 'react';
 import Loading from '@/src/components/globals/Loading';
@@ -58,39 +58,23 @@ export default async function Page({ params }: Props) {
 
   return (
     <Suspense fallback={<Loading />}>
-      <VisuallyHidden>
-        <h1 className={styles.title}>{project.title}</h1>
-      </VisuallyHidden>
-      <FadeIn>
-        <div className={styles.imageContainer}>
-          <Image
-            className={styles.image}
-            width={project.heroImage.width as number}
-            height={project.heroImage.height as number}
-            src={project.heroImage.url}
-            alt={project.heroImage.altText as string}
-            priority
-          />
-        </div>
+      <FadeIn className={styles.pageHeader}>
+        <h1>{project.title}</h1>
+        <FadeIn>
+          <div className={styles.imageContainer}>
+            <Image
+              className={styles.image}
+              width={project.heroImage.width as number}
+              height={project.heroImage.height as number}
+              src={project.heroImage.url}
+              alt={project.heroImage.altText as string}
+              priority
+            />
+          </div>
+        </FadeIn>
       </FadeIn>
-      <FadeIn>
-        <div className={styles.titleImageContainer}>
-          <img
-            className={styles.titleImage}
-            src={project.titleImage.url}
-            alt={project.titleImage.altText}
-            width={project.titleImage.width}
-            height={project.titleImage.height}
-          />
-        </div>
-      </FadeIn>
-      <FadeIn>
-        <ListBlock
-          className={styles.stacksList}
-          stacks={project.stacks}
-        />
-      </FadeIn>
-      <FadeIn>
+      <FadeIn className={styles.descriptionContainer}>
+        <h2>About</h2>
         <RichText content={project.description[0].Content} />
       </FadeIn>
       <Spacer height={30} />
--- src/app/(frontend)/work/[slug]/styles.module.scss.d.ts
+++ src/app/(frontend)/work/[slug]/styles.module.scss.d.ts
@@ -1,7 +1,8 @@
 export declare const arrowDown: string;
+export declare const descriptionContainer: string;
 export declare const image: string;
 export declare const imageContainer: string;
+export declare const pageHeader: string;
 export declare const stacksList: string;
-export declare const title: string;
 export declare const titleImage: string;
 export declare const titleImageContainer: string;
